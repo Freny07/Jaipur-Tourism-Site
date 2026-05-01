33e18a9b4eb330fe952d129e829d6f11d40d70c9
Freny07 <frenydpatel2007@gmail.com>
2026-05-02 01:10:59 +0530

    fix: make application extend SpringBootServletInitializer for external Tomcat deployment

--- jaipur-react/backend/src/main/java/com/jaipur/JaipurBackendApplication.java
+++ jaipur-react/backend/src/main/java/com/jaipur/JaipurBackendApplication.java
@@ -2,9 +2,17 @@ package com.jaipur;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JaipurBackendApplication {
+public class JaipurBackendApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JaipurBackendApplication.class);
+    }
+
 	public static void main(String[] args) {
 		SpringApplication.run(JaipurBackendApplication.class, args);
 	}
